45276a68b0f717da502ffc2cbe87fa4e1134b21c
SonjaKuv <kuvshinova.sonja@yandex.com>
2022-10-28 11:54:07 +0300

    dep1

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './pages/index.css';
 import App from '../src/components/App.js';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
